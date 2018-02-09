58c146cb5e096af9fc3df89fe4fd80b6bdcef198
Mohammed BOUTCHEKKOUCHT <boutchekkoucht@gmail.com>
2018-02-09 15:45:12 +0000

    initiate spring boot project

---
+++ src/main/java/com/consultant/management/ConsultantsManagementApplication.java
@@ -0,0 +1,12 @@
+package com.consultant.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultantsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultantsManagementApplication.class, args);
+	}
+}
---
+++ src/main/java/com/consultant/management/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.consultant.management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConsultantsManagementApplication.class);
+	}
+
+}
